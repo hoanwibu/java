df8f9c7803a81b5b6fbc4b639fd2c078433215e0
hoanwibu <mgniichan@gmail.com>
2024-05-26 13:19:19 +0700

    26/5

--- BaiNhapXuatMang.java
+++ BaiNhapXuatMang.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
-public class BaiNhapXuatMang{
-    public static void main(String args[]){
+
+public class BaiNhapXuatMang {
+    public static void main(String args[]) {
         System.out.print("Moi ban nhap so phan tu cua mang: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -9,62 +10,64 @@ public class BaiNhapXuatMang{
         System.out.println("Nhap mang: ");
         int max, min;
         min = max = arrInt[0];
-        for(int i = 0;i<n;i++){
-            System.out.print("This is arrInt["+i+"]: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print("This is arrInt[" + i + "]: ");
             arrInt[i] = sc.nextInt();
         }
-        //Min Max
-        for(int i = 0;i<n;i++){
-                if(arrInt[i]<min) min = arrInt[i];
-                if(arrInt[i]>max) max = arrInt[i];
+        // Min Max
+        for (int i = 0; i < n; i++) {
+            if (arrInt[i] < min)
+                min = arrInt[i];
+            if (arrInt[i] > max)
+                max = arrInt[i];
         }
         System.out.println();
         System.out.println("Xuat mang: ");
-        for(int i = 0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             System.out.println(arrInt[i]);
-         }
-         System.out.println();
-         System.out.println("Min: "+min+", "+"Max: "+ max);
-        //Tổng chắn lẻ của mảng
-         int S = 0, X = 0;
-        for(int i = 0;i<n;i++){
-           if(arrInt[i]%2 ==0){
-            S+=arrInt[i];
-           }
-           else X+=arrInt[i];
-    }
-    System.out.println();
-    System.out.println("Tong chan la: "+S+", "+"Tong le la: "+X);
-  // Thuật toán sắp xêp Bubble Sort
-   int t;
-   System.out.println();
-    System.out.println("Mang tang dan da sap xep lai la: ");
-    for(int i = 0;i<n;i++){
-        for(int j = n-1;j>i;j--){
-            if(arrInt[j-1]>arrInt[j]){
-                t = arrInt[j-1];
-                arrInt[j-1] = arrInt[j];
-                arrInt[j] = t;
+        }
+        System.out.println();
+        System.out.println("Min: " + min + ", " + "Max: " + max);
+        // Tổng chắn lẻ của mảng
+        int S = 0, X = 0;
+        for (int i = 0; i < n; i++) {
+            if (arrInt[i] % 2 == 0) {
+                S += arrInt[i];
+            } else
+                X += arrInt[i];
+        }
+        System.out.println();
+        System.out.println("Tong chan la: " + S + ", " + "Tong le la: " + X);
+        // Thuật toán sắp xêp Bubble Sort
+        int t;
+        System.out.println();
+        System.out.println("Mang tang dan da sap xep lai la: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j > i; j--) {
+                if (arrInt[j - 1] > arrInt[j]) {
+                    t = arrInt[j - 1];
+                    arrInt[j - 1] = arrInt[j];
+                    arrInt[j] = t;
+                }
             }
         }
-    }
-    for(int i = 0;i<n;i++){
-        System.out.println(" "+arrInt[i]);
-    }
-    System.out.println();
-    System.out.println("Mang giam dan da sap xep lai la: ");
-    for(int i = 0;i<n;i++){
-        for(int j = n-1;j>i;j--){
-            if(arrInt[j-1]<arrInt[j]){
-                t = arrInt[j-1];
-                arrInt[j-1] = arrInt[j];
-                arrInt[j] = t;
+        for (int i = 0; i < n; i++) {
+            System.out.println(" " + arrInt[i]);
+        }
+        System.out.println();
+        System.out.println("Mang giam dan da sap xep lai la: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j > i; j--) {
+                if (arrInt[j - 1] < arrInt[j]) {
+                    t = arrInt[j - 1];
+                    arrInt[j - 1] = arrInt[j];
+                    arrInt[j] = t;
+                }
             }
         }
+        for (int i = 0; i < n; i++) {
+            System.out.println(" " + arrInt[i]);
+        }
+        sc.close();
     }
-    for(int i = 0;i<n;i++){
-        System.out.println(" "+arrInt[i]);
-    }
-   // sc.close();
-}
 }
--- ptb2.java
+++ ptb2.java
@@ -24,4 +24,4 @@ public class ptb2 {
         }
         input.close();
     }
-    }
+}
